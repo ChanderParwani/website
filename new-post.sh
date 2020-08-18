aee9306cd50c1022e6d7e5cd727711e522799232
s-k-hassan <hassan.samuelk@gmail.com>
2020-08-18 10:55:34 +1000

    new post

---
+++ new-post.sh
@@ -0,0 +1,18 @@
+# Get date for post YYYY-MM-DD
+date --rfc-3339=date
+
+# Prompt for post title
+
+# Prompt for tags
+
+# Create adjusted post title with only letters, numbers and dashes
+
+# Create post in "posts" directory with format [YYYY-MM-DD][-][(adjusted post title)][.md]
+
+# Populate post with [dashes], [post], [post title], [date], [tags], [dashes]
+# [dashes]
+# [post]
+# [post title]
+date --rfc-3339=sec >> # [date]
+# [tags]
+# [dashes]
